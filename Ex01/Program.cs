4b4832fe61c38278d1d603291f4003b99c23000d
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-04 15:31:50 +0200

    Ajoutez des fichiers projet.

---
+++ Ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exo version Parse
+            Console.WriteLine("Veuillez entrer un premier nombre : ");
+            int nb1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez entrer un second nombre : ");
+            int nb2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{nb1} + {nb2} = {nb1 + nb2}");
+
+            //A ne pas reproduire...
+            //Console.WriteLine(int.Parse(Console.ReadLine()) + int.Parse(Console.ReadLine()));
+        }
+    }
+}
---
+++ Ex02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez entrer un premier nombre : ");
+            bool isConvertedNb1 = int.TryParse(Console.ReadLine(), out int nb1);
+            Console.WriteLine("Veuillez entrer un second nombre : ");
+            bool isConvertedNb2 = int.TryParse(Console.ReadLine(), out int nb2);
+            Console.WriteLine($"Le nombre 1 a bien été encodé? {isConvertedNb1}");
+            Console.WriteLine($"Le nombre 2 a bien été encodé? {isConvertedNb2}");
+            Console.WriteLine($"{nb1} + {nb2} = {nb1 + nb2}");
+        }
+    }
+}
