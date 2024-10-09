25d357e1ce7b93c260d42215f58243978fba0ae2
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-09 11:13:44 +0200

    Add Ex03

---
+++ Ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Veuillez indiquer un nombre (entier) : ");
+            string? userInput = Console.ReadLine();
+            bool isConverted = int.TryParse(userInput, out int userNb);
+            if (!isConverted) {
+                Console.WriteLine("On avait dit un nombre entier...");
+            }
+            else {
+                if(userNb / 2 + userNb / 2 == userNb)
+                {
+                    Console.WriteLine($"Le nombre {userNb} est pair.");
+                }
+                else
+                {
+                    Console.WriteLine($"Le nombre {userNb} est impair.");
+                }
+            }
+        }
+    }
+}
