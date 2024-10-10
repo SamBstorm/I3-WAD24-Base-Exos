2300b9a4c8c3fc7185ebcb39bee3eaa08161d6cd
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-10 09:40:58 +0200

    Add Correction Ex04

---
+++ Ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex04
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Veuillez introduire un premier nombre :");
+			string userInput = Console.ReadLine();
+			bool isConverted = int.TryParse(userInput, out int nb1);
+			if (!isConverted)
+			{
+				Console.WriteLine($"\"{userInput}\" n'est pas un nombre entier...");
+			}
+			else
+			{
+				Console.WriteLine("Veuillez introduire un second nombre :");
+				userInput = Console.ReadLine();
+				isConverted = int.TryParse(userInput, out int nb2);
+				if (!isConverted)
+				{
+					Console.WriteLine($"\"{userInput}\" n'est pas un nombre entier...");
+				}
+				else
+				{
+					/*int resultDivEntiere = nb1 / nb2;
+					Console.WriteLine($"Division entière : {resultDivEntiere}");*/
+					Console.WriteLine($"Division entière : {nb1 / nb2}");
+					Console.WriteLine($"Modulo : {nb1 % nb2}");
+					Console.WriteLine($"Division : {(float)nb1 / nb2}");
+				}
+			}
+		}
+	}
+}
