a17b552d9b5cd03a9f6016cf6c541410acb41084
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-10 11:45:03 +0200

    Add Correction Ex05 - BBAN

---
+++ Ex05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex05
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Veuillez introduire votre BBAN (12 chiffres): ");
+			string userInput = Console.ReadLine();
+			if(userInput.Length != 12 || !long.TryParse(userInput, out _))
+			{
+				Console.WriteLine("Entrée invalide");
+			}
+			else
+			{
+				string tenFirstChars = userInput.Substring(0,10);
+				string twoLastChars = userInput.Substring(10);
+				long tenFirst = long.Parse(tenFirstChars);
+				short twoLast = short.Parse(twoLastChars);
+				if( tenFirst % 97 == twoLast || (tenFirst % 97 == 0 && twoLast == 97))
+				{
+					Console.WriteLine("OK");
+				}
+				else
+				{
+					Console.WriteLine("KO");
+				}
+			}
+		}
+	}
+}
