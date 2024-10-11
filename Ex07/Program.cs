c90dc59977351617a9f96e998360a6efb22e3560
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-11 09:39:15 +0200

    Add correction Ex07 & Ex08 - Table de multiplication

---
+++ Ex07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex07
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int TABLE = 2;
+
+			/* WHILE */
+			int multiplicateur = 1;
+			while (multiplicateur <= 10)
+			{
+				Console.WriteLine($"{TABLE} x {multiplicateur} = {TABLE * multiplicateur}");
+				multiplicateur++;
+			}
+			
+			/*FOR
+			for (int multiplicateur = 1;multiplicateur <= 10;multiplicateur++)
+			{
+				Console.WriteLine($"{TABLE} x {multiplicateur} = {TABLE * multiplicateur}");
+			}
+			*/
+			/*DO...WHILE
+			int multiplicateur = 1;
+			do
+			{
+				Console.WriteLine($"{TABLE} x {multiplicateur} = {TABLE * multiplicateur}");
+				multiplicateur++;
+			} while (multiplicateur <= 10);*/
+		}
+	}
+}
---
+++ Ex08/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex08
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int table = 1;
+			while (table <= 9)
+			{
+				int multiplicateur = 1;
+				while (multiplicateur <= 10)
+				{
+					Console.WriteLine($"{table} x {multiplicateur} = {table * multiplicateur}");
+					multiplicateur++;
+				}
+				table++;
+			}
+		}
+	}
+}
