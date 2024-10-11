bf655b01b8ed8ee641d068f6d3b63bcabd974fec
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-11 14:18:40 +0200

    Add Correction Ex12 - Code Tiret Espace

---
+++ Ex12/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ex12
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int nbTiret;
+			do
+			{
+				Console.WriteLine("Veuillez introduire le nombre de tiret: ");
+			} while (!int.TryParse(Console.ReadLine(), out nbTiret) || nbTiret < 0);
+
+			int nbEspace;
+			do
+			{
+				Console.WriteLine("Veuillez introduire le nombre d'espace: ");
+			} while (!int.TryParse(Console.ReadLine(), out nbEspace) || nbEspace < 0);
+
+			int nbRepetition;
+			do
+			{
+				Console.WriteLine("Veuillez introduire le nombre de répétition: ");
+			} while (!int.TryParse(Console.ReadLine(), out nbRepetition) || nbRepetition < 0);
+
+			string tirets = "";
+			string espaces = "";
+
+			for (int i = 1; i <= nbTiret; i++) {
+				tirets += "-";
+			}
+
+			for (int i = 1; i <= nbEspace; i++)
+			{
+				espaces += " ";
+			}
+
+			for (int i = 1; i <= nbRepetition; i++) {
+				Console.Write($"{tirets}{espaces}");
+			}
+		}
+	}
+}
