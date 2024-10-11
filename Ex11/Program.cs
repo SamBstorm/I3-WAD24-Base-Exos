96425a2cf11aa1cbf843295199b55099f0418644
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-11 14:03:55 +0200

    Add correction Ex11 - Plus petit nombre

---
+++ Ex11/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex11
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int nb;
+			do
+			{
+				Console.WriteLine("Veuillez introduire un premier nombre : "); 
+			} while (!int.TryParse(Console.ReadLine(), out nb));
+
+			int smallest = nb;
+
+			for (int i = 1; i < 10; i++)
+			{
+				do
+				{
+					Console.WriteLine("Veuillez introduire un nouveau nombre : ");
+				} while (!int.TryParse(Console.ReadLine(), out nb));
+				if(nb < smallest)
+				{
+					smallest = nb;
+				}
+				//smallest = (nb < smallest) ? nb : smallest;
+			}
+			Console.WriteLine($"Le plus petit nombre de la série est {smallest}.");
+		}
+	}
+}
