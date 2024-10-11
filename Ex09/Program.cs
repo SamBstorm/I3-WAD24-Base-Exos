910810685290ca4b364991d3b0a51a62629a5d21
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-11 09:59:03 +0200

    Add Ex09 - Triangle d'⭐

---
+++ Ex09/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex09
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const string SYMBOL = "⭐";
+
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+			bool inputParsable;
+			int userNb = 0;
+			do
+			{
+				Console.WriteLine("Veuillez introduire un entier supérieur à 0 :");
+				string userInput = Console.ReadLine();
+				inputParsable = int.TryParse(userInput, out userNb);
+			} while (!inputParsable || userNb < 1);
+			
+			/*
+			string current = "";
+			for (int i = 1; i <= userNb; i++)
+			{
+				current += SYMBOL;
+				Console.WriteLine(current);
+			}*/
+			for(string current = ""; current.Length <= userNb; current+= SYMBOL)
+			{
+				Console.WriteLine(current);
+			}
+		}
+	}
+}
