4d16a5c122b002640718376857047394faa42614
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-14 15:26:20 +0200

    Add correction Ex13 - Tableau de 2 exposant i+1

---
+++ Ex13/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex13
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int SIZE = 10;
+			int[] tab = new int[SIZE];
+
+			/*tab[0] = 2;
+			for (int i = 1; i < tab.Length; i++) {
+				tab[i] = tab[i - 1] * 2;
+			}*/
+
+			for (int i = 0; i < tab.Length; i++) {
+				tab[i] = (int)Math.Pow(2, (i + 1));
+			}
+
+			foreach (int value in tab) {
+				Console.WriteLine(value);
+			}
+		}
+	}
+}
