f5ddacdeb64b741acb3e3a2ac4d7f4badd123b64
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-14 15:49:13 +0200

    Add Correction Ex14 - Tableau des scores

---
+++ Ex14/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Ex14
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int MINJOUEUR = 2;
+			const int MAXJOUEUR = 10;
+			int nbJoueurs;
+			do
+			{
+				Console.WriteLine($"Veuillez indiquer le nombre de joueur (de {MINJOUEUR} à {MAXJOUEUR} joueurs) :");
+			} while (!int.TryParse(Console.ReadLine(), out nbJoueurs) || nbJoueurs < MINJOUEUR || nbJoueurs > MAXJOUEUR);
+			int[] scores = new int[nbJoueurs];
+
+			for (int i = 0; i < nbJoueurs; i++)
+			{
+				do
+				{
+					Console.WriteLine($"Veuillez indiquer le score du joueur {i + 1} :");
+				} while (!int.TryParse(Console.ReadLine(), out scores[i]));
+			}
+
+			double total = 0;
+			for (int i = 0; i < nbJoueurs; i++) { 
+				total += scores[i];
+			}
+
+			double moyenne = total / nbJoueurs;
+
+			Console.WriteLine($"La moyenne des scores de nos {nbJoueurs} joueurs est de {moyenne}.");
+		}
+	}
+}
