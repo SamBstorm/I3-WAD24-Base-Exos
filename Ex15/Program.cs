de6b8b2f340b332c98d27f4e3ebe00dd1bc5fc23
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-14 16:12:27 +0200

    Add correction Ex15 - Tableau Inversé

---
+++ Ex15/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ex15
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random RNG = new Random();
+			const int SIZE = 7;
+			int[] tab = new int[SIZE];
+
+			for (int i = 0; i < SIZE; i++) {
+				tab[i] = RNG.Next(1,11);
+			}
+
+			Console.WriteLine("Tableau initial :");
+			foreach (int value in tab)
+			{
+				Console.WriteLine(value);
+			}
+			Console.WriteLine();
+
+			for (int i = 0; i < (SIZE/2) ; i++) {
+				int temp = tab[i];
+				tab[i] = tab[SIZE - 1 - i];
+				tab[SIZE - 1 - i] = temp;
+			}
+
+			Console.WriteLine("Tableau inversé :");
+			foreach (int value in tab)
+			{
+				Console.WriteLine(value);
+			}
+		}
+	}
+}
