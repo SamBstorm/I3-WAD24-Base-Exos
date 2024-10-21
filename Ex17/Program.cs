c6b972a5b9e09d4b3ec45fde2441efbfdfcef59a
Samuel Legrain <samuel.legrain@bstorm.be>
2024-10-21 14:28:17 +0200

    Add Correction Ex17 - Tableau Pion

---
+++ Ex17/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Ex17
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.Unicode;
+			
+			//Initialisation
+			bool[] tab = new bool[10];
+			//int index = 0;
+			tab[0] = true;
+
+			string userChoice;
+
+			
+			do
+			{
+				//Affichage
+				Console.Clear();
+				for (int i = 0; i < tab.Length; i++)
+				{
+					if (tab[i])
+					{
+						Console.Write("😅");
+					}
+					else
+					{
+						Console.Write("⏹️");
+					}
+				}
+				Console.WriteLine();
+
+				//Menu de choix
+				Console.WriteLine("(G)auche | (D)roite | (Q)uitter");
+				userChoice = Console.ReadLine().ToUpper();
+				int index = Array.IndexOf(tab, true);
+
+				//Déplacement
+				tab[index] = false;
+				if (userChoice == "G" && index > 0)
+				{
+					index--;
+				}
+				else if (userChoice == "D" && index < tab.Length - 1)
+				{
+					index++;
+				}
+				tab[index] = true;
+
+			} while (userChoice != "Q");
+		}
+	}
+}
