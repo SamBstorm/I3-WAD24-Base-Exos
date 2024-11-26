2ad4fd07ebf73b53a41579ea24a8dc0704c9c326
Samuel Legrain <samuel.legrain@bstorm.be>
2024-11-26 20:17:36 +0100

    Add Ex23 - Enum ConsoleAvancee

---
+++ Ex23/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleAvancee ca = new ConsoleAvancee();
+            ca.RainbowWrite("Hello, wad24! Here's World!");
+        }
+    }
+}
---
+++ Ex23/Structs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex23
+{
+    public struct ConsoleAvancee
+    {
+        public void RainbowWrite(string texte)
+        {
+            const int NB_COLORS = 16;
+            for (int i = 0; i < texte.Length; i++)
+            {
+                char letter = texte[i];
+                Console.ForegroundColor = (ConsoleColor)(i % NB_COLORS);
+                Console.BackgroundColor = (ConsoleColor)(NB_COLORS - 1 - (i % NB_COLORS));
+                Console.Write(letter);
+            }
+            Console.ResetColor();
+        }
+    }
+}
